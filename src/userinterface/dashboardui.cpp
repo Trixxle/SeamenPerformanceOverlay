7eedf44ba1c8177992ea8aeffcbaedaa50b4980e
jOrn <37781697+Trixxle@users.noreply.github.com>
2026-02-03 20:09:40 +0100

    IT WORKS
    IT FINALLY FUCKING WORKS

--- src/userinterface/dashboardui.cpp
+++ src/userinterface/dashboardui.cpp
@@ -2,8 +2,11 @@
 // Created by jornt on 03/02/2026.
 //
 
+// You may need to build the project (run Qt uic code generator) to get "ui_dashboardui.h" resolved
+
 #include "dashboardui.h"
 #include "ui_dashboardui.h"
+
 
 dashboardui::dashboardui(QWidget *parent) : QWidget(parent), ui(new Ui::dashboardui) {
     ui->setupUi(this);
