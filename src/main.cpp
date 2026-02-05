dbb337846a8853ac658e671b88c3ae12a0bb54ec
jOrn <37781697+Trixxle@users.noreply.github.com>
2026-02-05 17:09:48 +0100

    OpenVR test code
    Doesnt work, cant find openvr_api.dll

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,46 @@
 #include "userinterface/dashboardui.h"
+#include "openvr.h"
 
 #include <QApplication>
+
+QString GetTrackedDeviceString( vr::IVRSystem *pHmd, vr::TrackedDeviceIndex_t unDevice, vr::TrackedDeviceProperty prop )
+{
+    char buf[128];
+    vr::TrackedPropertyError err;
+    pHmd->GetStringTrackedDeviceProperty( unDevice, prop, buf, sizeof( buf ), &err );
+    if( err != vr::TrackedProp_Success )
+    {
+        return QString( "Error Getting String: " ) + pHmd->GetPropErrorNameFromEnum( err );
+    }
+    else
+    {
+        return buf;
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    dashboardui w;
-    w.show();
-    return a.exec();
+
+    vr::HmdError m_eLastHmdError = vr::VRInitError_None;
+    QString m_strVRDriver;
+    QString m_strVRDisplay;
+    vr::IVRSystem *pVRSystem = vr::VR_Init( &m_eLastHmdError, vr::VRApplication_Overlay );
+
+        if ( m_eLastHmdError != vr::VRInitError_None )
+        {
+            m_strVRDriver = "No Driver";
+            m_strVRDisplay = "No Display";
+            return false;
+        }
+
+        m_strVRDriver = GetTrackedDeviceString(pVRSystem, vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_TrackingSystemName_String);
+        m_strVRDisplay = GetTrackedDeviceString(pVRSystem, vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_SerialNumber_String);
+
+
+    return 0;
+    //QApplication a(argc, argv);
+    //dashboardui w;
+    //w.show();
+    //return a.exec();
 }
